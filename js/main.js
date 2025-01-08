f4ff261574156d9f7e7c10e3cc7ca6b0ec821a9a
Degergokalp <86526625+Degergokalp@users.noreply.github.com>
2025-01-09 01:10:28 +0300

    init

--- js/main.js
+++ js/main.js
@@ -1,1 +1,38 @@
-// No JavaScript was present at this point 
+// Your previous JavaScript code here
+// (Empty if there wasn't any) 
+
+// Initialize Swiper
+const resultsSwiper = new Swiper('.results-slider', {
+    slidesPerView: 3,
+    centeredSlides: true,
+    loop: true,
+    speed: 1000,
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+    },
+    effect: 'coverflow',
+    coverflowEffect: {
+        rotate: 0,
+        stretch: 0,
+        depth: 100,
+        modifier: 2,
+        slideShadows: false,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    breakpoints: {
+        320: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 3,
+        }
+    }
+}); 
